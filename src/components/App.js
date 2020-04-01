d53536f185e03086400309e55bcd0d2531b5a354
Jessica Leite <jessicaleite@MacBook-Pro-de-Jessica.local>
2020-04-01 16:33:30 -0300

    displayed state with JSX and Updated our Order State

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import samplesFishes from "../sample-fishes";
+import Fish from './Fish';
 
 class App extends React.Component{
 
@@ -25,11 +26,31 @@ class App extends React.Component{
         this.setState( {fishes: samplesFishes} )
     };
 
+    addToOrder= (key) => {
+        // 1. take a copy of state
+        const order = { ...this.state.order };
+
+        // 2. Either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+
+        // 3. Call setState to update our state object
+        this.setState({ order })
+    }
+
     render(){
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => 
+                            <Fish 
+                                key={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder}
+                                index={key}
+                            />)}
+                    </ul>
                 </div>
                 <Order/>
                 <Inventory 
